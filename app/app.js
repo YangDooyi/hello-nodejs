0ff622496b7baa76b081bde4bd177473bb27fca1
Kim, InGee <ingee@paran.com>
2016-10-21 13:02:02 +0900

    show network device info

--- app/app.js
+++ app/app.js
@@ -2,7 +2,18 @@ var http = require('http');
 var os = require('os');
 
 var msg = '<h1>Hello World, my name is ' + os.hostname() +'</h1>';
-  + '<p><ul>';
+msg += '<p><ul>';
+var ifaces = os.networkInterfaces();
+var ifnames = Object.keys(ifaces);
+for (var ifname of ifnames) {
+  var ifaceSettings = ifaces[ifname];
+  for (var iface of ifaceSettings) {
+    if (iface.family === 'IPv4' && iface.internal === false) {
+      msg += '<li>' + ifname + ': ' + iface.address + '</li>';
+    }
+  }
+}
+msg += '</ul></p>';
 var server = http.createServer(function(request, response) {
     response.end(msg);
 });
