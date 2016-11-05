5667cc324e733995ea004352d7858247932c6d99
InGee Kim <ingee.kim@gmail.com>
2016-11-06 00:12:45 +0900

    make call to api-server when web-server is ready

--- app/app.js
+++ app/app.js
@@ -1,6 +1,25 @@
 var http = require('http');
 var os = require('os');
 
+//
+// report alive to api_server
+//
+
+var api_server = process.env['API_SERVER'];
+if (api_server) {
+  console.log('api_server ip= ' + api_server);
+  http.request({ host:api_server, port:8001, path:'/done', method:'GET' }, 
+    function(res) {
+      console.log('api_server returns status-code ' + res.statusCode);
+    }).on('error', function(e) {
+      console.log('api_server fail - ' + e);
+    }).end();
+}
+
+//
+// show network interface info to Web
+//
+
 var msg = '<html><body>';
 msg += '<h1>Hello World,<br/>my hostname is ' + os.hostname() +'</h1>';
 msg += '<p><ul>';
@@ -21,5 +40,6 @@ var server = http.createServer(function(request, response) {
 });
 
 var port = 8000;
-server.listen(port);
-console.log("listening at " + port + "...");
+server.listen(port, function() {
+  console.log("web server is ready on port " + port);
+});
